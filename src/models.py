db147c979101d2afc0bbc04f7f55a8c054f8c186
bobo305 <126503694+bobo305@users.noreply.github.com>
2023-08-16 14:54:06 +0000

    almost done need to render diagram

--- src/models.py
+++ src/models.py
@@ -1,37 +1,65 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
-Base = declarative_base()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+db = SQLAlchemy(app)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    firstName = db.Column(db.String(250))
+    lastName = db.Column(db.String(250))
 
-    def to_dict(self):
-        return {}
+class Followers(db.Model):
+    __tablename__ = 'followers'
+    user_from_id= db.Column(db.Integer, db.ForeignKey('User.id'))
+    user_to_id= db.Column(db.Integer, db.ForeignKey('User.id'))
+   
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(db.Integer, db.ForeignKey('User.id'))
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+class Media(db.Model):
+    __tablename__ = 'media'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Enum)
+    url = db.Column(db.String(250))
+    post_id= db.Column(db.Integer, db.ForeignKey('Post.id'))
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(250), nullable=True)
+    author_id= db.Column(db.Integer, db.ForeignKey('User.id'))
+    post_id= db.Column(db.Integer, db.ForeignKey('Poat.id'))
+
+
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+
+    user = db.relationship('User', back_populates='favorites')
+    planet = db.relationship('Planet', back_populates='favorites')
+    character = db.relationship('Character', back_populates='favorites')
+
+
+User.favorites = db.relationship('Favorite', back_populates='user')
+Planet.favorites = db.relationship('Favorite', back_populates='planet')
+Character.favorites = db.relationship('Favorite', back_populates='character')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run()

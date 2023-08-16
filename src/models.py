5ce16c15f1c7219b2924dfd2c99ec4463af0aee3
bobo305 <126503694+bobo305@users.noreply.github.com>
2023-08-16 16:43:03 +0000

    almsot done with relationships

--- src/models.py
+++ src/models.py
@@ -1,5 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from typing import List
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -15,49 +20,37 @@ class User(db.Model):
     password = db.Column(db.String(250), nullable=False)
     firstName = db.Column(db.String(250))
     lastName = db.Column(db.String(250))
+    folowers_column: Mapped[List["Followers"]] = relationship()
 
 class Followers(db.Model):
     __tablename__ = 'followers'
-    user_from_id= db.Column(db.Integer, db.ForeignKey('User.id'))
-    user_to_id= db.Column(db.Integer, db.ForeignKey('User.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    User_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
    
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
-    user_id= db.Column(db.Integer, db.ForeignKey('User.id'))
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Media(db.Model):
     __tablename__ = 'media'
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.Enum)
+    type = db.Column(db.String(250))
     url = db.Column(db.String(250))
-    post_id= db.Column(db.Integer, db.ForeignKey('Post.id'))
+    post_id= db.Column(db.Integer, db.ForeignKey('post.id'))
 
 
 class Comment(db.Model):
     __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(250), nullable=True)
-    author_id= db.Column(db.Integer, db.ForeignKey('User.id'))
-    post_id= db.Column(db.Integer, db.ForeignKey('Poat.id'))
+    author_id= db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id= db.Column(db.Integer, db.ForeignKey('post.id'))
 
 
 
-class Favorite(db.Model):
-    __tablename__ = 'favorite'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
-    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
 
-    user = db.relationship('User', back_populates='favorites')
-    planet = db.relationship('Planet', back_populates='favorites')
-    character = db.relationship('Character', back_populates='favorites')
-
-
-User.favorites = db.relationship('Favorite', back_populates='user')
-Planet.favorites = db.relationship('Favorite', back_populates='planet')
-Character.favorites = db.relationship('Favorite', back_populates='character')
 
 if __name__ == '__main__':
     with app.app_context():
